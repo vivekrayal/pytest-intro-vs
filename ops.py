b68bcfd865a5af5469bac2ce9d1ca2d93ae32d24
vivekrayal <115876604+vivekrayal@users.noreply.github.com>
2022-12-28 23:47:19 +0530

    Update ops.py

--- ops.py
+++ ops.py
@@ -1,11 +1,11 @@
-def add(x , y):
+def add(x, y):
     return x+y
 
-def subtract(x , y):
+def subtract(x, y):
     return x-y
 
-def multiply(x , y):
+def multiply(x, y):
     return x*y
 
-def divide(x , y):
+def divide(x, y):
     return x/y
