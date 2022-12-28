a74c8aa2332b63b9aa1805a26ba481331afce764
vivekrayal <115876604+vivekrayal@users.noreply.github.com>
2022-12-29 00:04:40 +0530

    Update ops.py

--- ops.py
+++ ops.py
@@ -1,5 +1,12 @@
 """Module docs."""
 # pylint: disable=too-few-public-methods, invalid-name
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-function-docstring
+# pylint: disable=invalid-name
+# pylint: disable=trailing-newlines
+# pylint: disable=trailing-whitespace
+# pylint: disable=missing-final-newline
 
 def add(xvar, yvar):
     return xvar+yvar
